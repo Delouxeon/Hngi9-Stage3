df5f81c9c6a3073d2b5f8aa67a7962187c5a9a5f
delouxeon <oluxnwabuife@gmail.com>
2022-11-16 10:47:12 +0100

    style:location bar styles changed

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import './App.css';
 import {Navigate, Routes} from "react-router-dom";
 import {Route} from "react-router-dom";
-import Home from './Indexpage';
+import Home from './Indexpage/index'
 import Places from './PlacesToStay/Places';
 import ConnectWallet from './Connect-Wallet/connectWallet';
 
