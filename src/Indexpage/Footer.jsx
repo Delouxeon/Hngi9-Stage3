3a08050aa5864a46627c88d3508b3019652ecf17
delouxeon <oluxnwabuife@gmail.com>
2022-11-17 06:50:56 +0100

    fix: fontsize and alignment issues fixed

--- src/Indexpage/Footer.jsx
+++ src/Indexpage/Footer.jsx
@@ -9,43 +9,45 @@ const Footer = () => {
 
 return(
   <footer className='footer'>
-    <div className='footer-container'>
-      <div>
-        <img className ='footermethabnb' src={FooterMetabnb} />
-        <div className='footer-icons'>
-          <img className='facebook' src={facebook} />
-          <img className='instagram' src={instagram} />
-          <img className='twitter' src={twitter} />
+    <div className='footer-content'>
+      <div className='footer-container'>
+        <div>
+          <img className ='footermethabnb' src={FooterMetabnb} />
+          <div className='footer-icons'>
+            <img className='facebook' src={facebook} />
+            <img className='instagram' src={instagram} />
+            <img className='twitter' src={twitter} />
+          </div>
+        </div>  
+        <div>
+          <h4>Comminity</h4>
+          <ul>
+            <li><a href="">NFT</a></li>
+            <li><a href="">Tokens</a></li>
+            <li><a href="">Lanlords</a></li>
+            <li><a href="">Discord</a></li>
+          </ul>
         </div>
-        <p>&copy; 2022 Metabnb</p>
+        <div>
+          <h4>Places</h4>
+          <ul>
+            <li><a href="">Castle</a></li>
+            <li><a href="">Farms</a></li>
+            <li><a href="">Beach</a></li>
+            <li><a href="">Learn more</a></li>
+          </ul>
+        </div>
+        <div>
+          <h4>About us</h4>
+          <ul>
+            <li><a href="">Road map</a></li>
+            <li><a href="">Creators</a></li>
+            <li><a href="">Career</a></li>
+            <li><a href="">Contact us</a></li>
+          </ul>
+        </div>
       </div>
-      <div>
-        <h4>Comminity</h4>
-        <ul>
-          <li><a href="">NFT</a></li>
-          <li><a href="">Tokens</a></li>
-          <li><a href="">Lanlords</a></li>
-          <li><a href="">Discord</a></li>
-        </ul>
-      </div>
-      <div>
-        <h4>Places</h4>
-        <ul>
-          <li><a href="">Castle</a></li>
-          <li><a href="">Farms</a></li>
-          <li><a href="">Beach</a></li>
-          <li><a href="">Learn more</a></li>
-        </ul>
-      </div>
-      <div>
-        <h4>About us</h4>
-        <ul>
-          <li><a href="">Road map</a></li>
-          <li><a href="">Creators</a></li>
-          <li><a href="">Career</a></li>
-          <li><a href="">Contact us</a></li>
-        </ul>
-      </div>
+      <p className='copyright' >&copy; 2022 Metabnb</p> 
     </div>
   </footer>
 )
